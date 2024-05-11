7f2a7b5d004d418a1665d4f7a1bde2d7905ab38e
kapepa <kapepayx@gmail.com>
2024-05-11 11:20:10 +0300

    info card

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -10,6 +10,7 @@ import { cn } from "@/lib/utils";
 import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
 import { Header, HeaderSkeleton } from "./header";
+import { InfoCard } from "./info-card";
 
 interface StreamPlayerProps {
   player: User,
@@ -61,6 +62,12 @@ const StreamPlayer: FC<StreamPlayerProps> = (props) => {
             viewerIdentity={identity}
             isFollowing={isFollowing}
             name={stream.name}
+          />
+          <InfoCard
+            name={stream.name}
+            hostIdentity={player.id || ""}
+            thumbnailUrl={stream.thumbnailUrl}
+            viewerIdentity={identity}
           />
         </div>
         <div
